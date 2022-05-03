8739f752fe00cab866e1fccabadec123bfcfb121
amyleblanc <amymccarthy2014@gmail.com>
2022-05-03 16:17:08 -0700

    setup Application component

---
+++ react-front-end/src/components/Application.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Application() {
+  return (
+    <main>
+      <section>
+        <div>
+          <h1>Pro-Bone-O</h1>
+        </div>
+      </section>
+    </main>
+  );
+}

8b283fcea9cf2e7291e982fd463dc934478e3fbe
brysonbest <brycekalden@gmail.com>
2022-05-04 21:08:03 -0700

    Seeded prisma database and updated schema.

--- express-back-end/db/seeds/01_users.sql
+++ express-back-end/db/seeds/01_users.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (first_name, last_name, email_address, password, phone_number, postal_code, photo_url, rating, is_dog_owner) VALUES
 ('Rhys', 'Wood', 'rhys@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5J5T3', 'https://randomuser.me/api/portraits/men/94.jpg', 4.3, TRUE),
-('Amy' 'McCarthy', 'Amy@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5T4G6', 'https://randomuser.me/api/portraits/women/44.jpg', 4.0, TRUE),
+('Amy', 'McCarthy', 'Amy@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5T4G6', 'https://randomuser.me/api/portraits/women/44.jpg', 4.0, TRUE),
 ('Bryson', 'Best', 'BrysonB@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V8T4P1', 'https://randomuser.me/api/portraits/men/88.jpg', 4.9, TRUE),
 ('Chloe', 'Dace', 'chloe457@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5K2R4', 'https://randomuser.me/api/portraits/women/72.jpg', 4.8, FALSE),
 ('Katy', 'Witts', 'katy425@me.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V4R7Y6', 'https://randomuser.me/api/portraits/women/68.jpg', 4.4, FALSE),
-('Luke', 'Bradshaw', 'luke420420@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V6Y9I8', 'https://randomuser.me/api/portraits/men/40.jpg', 3.5 FALSE),
+('Luke', 'Bradshaw', 'luke420420@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V6Y9I8', 'https://randomuser.me/api/portraits/men/40.jpg', 3.5, FALSE),
 ('JJ', 'Coates', 'JJ254@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V4R6T6', 'https://randomuser.me/api/portraits/men/82.jpg', 2.4, FALSE),
 ('Catherine', 'Ewusak', 'cath422@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5Y5T5', 'https://randomuser.me/api/portraits/women/64.jpg', 4.8, FALSE),
 ('Ben', 'Selbie', 'Ben4525@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5K2T6', 'https://randomuser.me/api/portraits/men/62.jpg', 5.0, TRUE),
 ('Ash', 'Mason', 'ashleh157@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V4T7H7', 'https://randomuser.me/api/portraits/men/59.jpg', 2.2, FALSE),
 ('Josh', 'Williams', 'josh1354@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V4T7T3', 'https://randomuser.me/api/portraits/men/11.jpg', 3.6, FALSE),
 ('Robert', 'Booth', 'rob4275@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V5T0B6', 'https://randomuser.me/api/portraits/men/52.jpg', 4.0, FALSE),
-('Thomas', 'Dunn', 'tom422@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V3T8Y6', 'https://randomuser.me/api/portraits/men/34.jpg', 3.8, FALSE),
+('Thomas', 'Dunn', 'tom422@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 7783586873, 'V3T8Y6', 'https://randomuser.me/api/portraits/men/34.jpg', 3.8, FALSE);

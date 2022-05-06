c1e043d1d30fdd930f0565744f356e48b6ad0271
RhysWood <rhyswoody@gmail.com>
2022-05-05 20:17:44 -0700

    add pets seeds

---
+++ express-back-end/db/seeds/03_pets.sql
@@ -0,0 +1,17 @@
+INSERT INTO pets (user_id, name, photo_url, breed, description, difficulty) VALUES
+(1,'Alfie', 'https://images.dog.ceo/breeds/affenpinscher/n02110627_6069.jpg', 'Saint Bernard', 'Friendly, Lively, Gentle, Watchful, Calm', 4),
+(2,'Akita', 'https://images.dog.ceo/breeds/akita/512px-Akita_inu.jpeg', 'Shiba Inu', 'Charming, Fearless, Keen, Alert, Confident, Faithful', 2),
+(3,'Borzio', 'https://images.dog.ceo/breeds/borzoi/n02090622_3210.jpg', 'Chinese Crested', 'Affectionate, Sweet-Tempered, Lively, Alert, Playful, Happy', 2),
+(4,'Banchee', 'https://images.dog.ceo/breeds/beagle/n02088364_14394.jpg', 'Border Collie', 'Tenacious, Keen, Energetic, Responsive, Alert, Intelligent', 3),
+(5,'Charlie', 'https://images.dog.ceo/breeds/pembroke/n02113023_5901.jpg', 'Pembrook', 'Benevolent, Agile, Alert, Intelligent, Active, Territorial', 4),
+(6,'Mac', 'https://images.dog.ceo/breeds/pinscher-miniature/n02107312_105.jpg', 'Miniature Pinscher', 'Docile, Friendly, Affectionate, Loyal, Patient, Gentle', 1),
+(7,'Molly', 'https://images.dog.ceo/breeds/poodle-standard/n02113799_4904.jpg', 'Standard Poodle', 'Tenacious, Keen, Energetic, Responsive, Alert, Intelligent', 3),
+(8,'Zach', 'https://images.dog.ceo/breeds/pointer-german/n02100236_5643.jpg', 'German Pointer', 'Protective, Loyal, Independent, Intelligent, Loving, Familial', 5),
+(9,'Maverick', 'https://images.dog.ceo/breeds/labrador/n02099712_610.jpg', 'Labrador', 'Strong Willed, Kind, Loyal, Intelligent, Dominant, Powerful', 2),
+(10,'Max', 'https://images.dog.ceo/breeds/mastiff-english/4.jpg', 'Engish Mastiff', 'Trainable, Protective, Sweet-Tempered, Keen, Active', 5),
+(11,'Lucy', 'https://images.dog.ceo/breeds/mountain-swiss/n02107574_1441.jpg', 'Swiss Mountain', 'Aloof, Clownish, Dignified, Independent, Happy', 2),
+(12,'Henry', 'https://images.dog.ceo/breeds/redbone/n02090379_3881.jpg', 'Red Bone', 'Amiable, Even Tempered, Excitable, Determined, Gentle, Intelligent', 1),
+(13,'Rooney', 'https://images.dog.ceo/breeds/hound-plott/hhh-23456.jpeg', 'Plott Hound', 'Trainable, Friendly, Affectionate, Playful, Quiet, Faithful', 2),
+(14,'Bobby', 'https://images.dog.ceo/breeds/saluki/n02091831_2245.jpg', 'Saluki', 'Spirited, Agile, Loyal, Gentle, Active, Courageous', 5)
+
+

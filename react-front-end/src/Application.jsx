0e1d6a67026e09e33d858291584883d3bdae2e23
amyleblanc <amymccarthy2014@gmail.com>
2022-05-07 14:38:44 -0700

    add mui components to create basic home page test

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -1,13 +1,56 @@
 import React from "react";
+import { 
+  AppBar,
+  Button,
+  Grid,
+  Toolbar,
+  Typography,
+  Container
+} from "@material-ui/core";
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import useStyles from "./styles/ApplicationStyles";
+import "./Application.css";
 
 export default function Application() {
+  const styles = useStyles();
   return (
-    <main>
-      <section>
-        <div>
-          <h1>Pro-Bone-O</h1>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <img src="/images/pro-bone-o_logo.png" className={styles.icon}/>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={styles.container}>
+          <Container maxWidth="sm" style={{ marginTop: '100px'}}>
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Pro-Bone-O
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Find free dog walkers & sitters in your area!
+            </Typography>
+            <div className={styles.buttonContainer}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button className={styles.loginButton} variant="contained">
+                    Login
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button className={styles.registerButton}  variant="outlined">
+                    Register
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
         </div>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }

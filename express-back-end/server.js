158f350a9d8cdc2f95835ab5c6a4d5152bb9dedd
RhysWood <rhyswoody@gmail.com>
2022-05-07 13:25:32 -0700

    add hard coded data

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -9,9 +9,34 @@ app.use(BodyParser.json());
 app.use(express.static('public'));
 
 // Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+app.get('/api/data', (req, res) => {
+  const users = [
+    {
+    first_name: 'Rhys', 
+    last_name: 'Wood', 
+    email_address:'rhys@mail.com', 
+    password: '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 
+    phone_number: 7783586873, 
+    postal_code: 'V5J5T3', 
+    photo_url: 'https://randomuser.me/api/portraits/men/94.jpg', 
+    rating: 4.3,
+    is_dog_owner: true
+  },
+    {
+    first_name: 'Rhys', 
+    last_name: 'Wood', 
+    email_address:'rhys@mail.com', 
+    password: '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 
+    phone_number: 7783586873, 
+    postal_code: 'V5J5T3', 
+    photo_url: 'https://randomuser.me/api/portraits/men/94.jpg', 
+    rating: 4.3,
+    is_dog_owner: true
+    }
+  ]
+
+  res.json(users)
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console

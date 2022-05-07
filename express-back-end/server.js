bbddf3c71b808a14790a342a6fa225293697ec9d
RhysWood <rhyswoody@gmail.com>
2022-05-07 12:46:32 -0700

    de-capitalize express and app constants in server

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,19 +1,20 @@
-const Express = require('express');
-const App = Express();
+const express = require('express');
+const app = express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+// express Configuration
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json());
+app.use(express.static('public'));
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
+app.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
-App.listen(PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+

61fb9a0b3435f581600eacf825c950d8e1585a2e
brysonbest <brycekalden@gmail.com>
2022-05-07 21:50:19 -0700

    Prettier auto-format.

--- react-front-end/src/components/listing.js
+++ react-front-end/src/components/listing.js
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react';
-const axios = require('axios').default;
-
+import React, { useEffect, useState } from "react";
+const axios = require("axios").default;
 
 export default function Listing() {
   const [listing, setListing] = useState([]);
 
   useEffect(() => {
     const listings = async () => {
-      const res = await axios('/api/listing')
-      setListing(res.data)
+      const res = await axios("/api/listing");
+      setListing(res.data);
     };
     listings();
   }, []);
 
-  const useListing = listing.map((listing)=>{
-    return <div key={listing.id}>
-                <h3>{listing.id}</h3>
-                <h4>{listing.activity_type}</h4>
-                <p>{listing.additional_details}</p>
-              </div> 
-    })
+  const useListing = listing.map((listing) => {
+    return (
+      <div key={listing.id}>
+        <h3>{listing.id}</h3>
+        <h4>{listing.activity_type}</h4>
+        <p>{listing.additional_details}</p>
+      </div>
+    );
+  });
 
   return (
     <main>
       <section>
         <div>
           <h1>Current Listings</h1>
-          <div className="container">
-          {listing && useListing}
-      </div>
+          <div className="container">{listing && useListing}</div>
         </div>
       </section>
     </main>

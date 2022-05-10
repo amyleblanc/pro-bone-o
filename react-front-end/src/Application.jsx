67e706a598d992b17d1581f36f266716f7ed102b
brysonbest <brycekalden@gmail.com>
2022-05-09 19:37:40 -0700

    Update pet registration component.

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -7,18 +7,20 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import ResponsiveAppBar from "./components/NavBar";
 import Splash from "./pages/splash";
+import RegisterPet from "./components/RegisterPet";
 
 export default function Application() {
   return (
     <>
       <main>
         <BrowserRouter>
-            <CssBaseline />
-            <ResponsiveAppBar />
+          <CssBaseline />
+          <ResponsiveAppBar />
           <Routes>
             <Route path="/" element={<Splash />} />
             <Route path="/listing" element={<Listing />} />
             <Route path="/createlisting" element={<ListingForm />} />
+            <Route path="/registerPet" element={<RegisterPet />} />
           </Routes>
         </BrowserRouter>
       </main>

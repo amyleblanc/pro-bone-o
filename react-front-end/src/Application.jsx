f24a19ea82af28382cbd5e1a75dc230da11e8c6c
amyleblanc <amymccarthy2014@gmail.com>
2022-05-11 00:04:50 -0700

    add API URL prop to Profile component

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -45,7 +45,7 @@ export default function Application() {
             <Route path="/registerUser" element={<RegisterUser />} />
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile url={"/api/users/1"} />} /> {/* Hard-coded to user 1 */}
           </Routes>
         </BrowserRouter>
       </main>

e4fd6cade28eb2bceacb71075b5cf4ebcfda6ffe
amyleblanc <amymccarthy2014@gmail.com>
2022-05-10 19:36:57 -0700

    add '/profile' route; rearrange imports by type

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import ResponsiveAppBar from "./components/NavBar";
 import Listing from "./components/Listing";
 import ListingForm from "./components/form";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import CssBaseline from "@mui/material/CssBaseline";
-
-import ResponsiveAppBar from "./components/NavBar";
 import Splash from "./pages/splash";
+import Profile from "./pages/Profile"
 
 export default function Application() {
   return (
@@ -19,6 +18,7 @@ export default function Application() {
             <Route path="/" element={<Splash />} />
             <Route path="/listing" element={<Listing />} />
             <Route path="/createlisting" element={<ListingForm />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </BrowserRouter>
       </main>

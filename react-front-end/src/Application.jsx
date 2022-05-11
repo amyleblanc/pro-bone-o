e67b7a055a61a6db119ed2275fe7a9c6d546251f
RhysWood <rhyswoody@gmail.com>
2022-05-10 18:30:19 -0700

    add user atom and user state

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Listing from "./components/Listing";
 import ListingForm from "./components/form";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import { atom, useRecoilState } from "recoil";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 import ResponsiveAppBar from "./components/NavBar";
 import Splash from "./pages/splash";
+const axios = require("axios").default;
+
+const userState = atom({
+  key: "user",
+  default: [],
+})
 
 export default function Application() {
+  const [user, setUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get(`/login/1`);
+      setUser(res.data); 
+    }
+  }, [])
+
   return (
     <>
       <main>

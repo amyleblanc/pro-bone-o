89e71cb5e84ea35e02daa4cc8440d5ac7f1d20a8
RhysWood <rhyswoody@gmail.com>
2022-05-11 19:28:50 -0700

    logout now clears global state

--- react-front-end/src/components/Logout.jsx
+++ react-front-end/src/components/Logout.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { useRecoilState } from "recoil";
+import userState from "./atoms";
 
 const axios = require("axios").default;
 
 
 export default function Logout() {
+
+const [user, setUser] = useRecoilState(userState);
+
+setUser('')
     axios.get('/logout')
     return (
         <>

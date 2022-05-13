2465ea9737ef21104c711e91a489f0c2afd91787
amyleblanc <amymccarthy2014@gmail.com>
2022-05-12 22:59:03 -0700

    add myListings/myBookings routes; organize imports

--- react-front-end/src/Application.jsx
+++ react-front-end/src/Application.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Listing from "./components/Listing";
 import ListingForm from "./components/form";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import RegisterPet from "./components/RegisterPet";
 import RegisterUser from "./components/RegisterUser";
+import Splash from "./pages/splash";
 import Profile from "./pages/Profile";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyListings from "./pages/MyListings";
+import MyBookings from "./pages/MyBookings";
 import CssBaseline from "@mui/material/CssBaseline";
 import ResponsiveAppBar from "./components/NavBar";
-import Splash from "./pages/splash";
-const axios = require("axios").default;
-
-
 
 export default function Application() {
- 
+  
   return (
     <>
       <main>
@@ -31,6 +30,8 @@ export default function Application() {
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/mylistings" element={<MyListings url={"/api/listing"} />} />
+            <Route path="/mybookings" element={<MyBookings />} />
           </Routes>
         </BrowserRouter>
       </main>

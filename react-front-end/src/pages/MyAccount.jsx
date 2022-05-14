722ec3ab66c9987d9555715f5a85d336deb86ee9
amyleblanc <amymccarthy2014@gmail.com>
2022-05-14 15:32:52 -0700

    create My Account component to house NavTabs

---
+++ react-front-end/src/pages/MyAccount.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { Typography, Container } from "@mui/material/";
+import userState from "../components/atoms";
+import NavTabs from "../components/Tabs";
+
+export default function MyAccount() {
+  const user = useRecoilValue(userState);
+
+  return (
+    <div>
+      <Container maxWidth="sm" style={{ marginTop: "100px" }}>
+        <Typography
+          paddingBottom="40px"
+          variant="h3"
+          align="center"
+          color="textSecondary"
+        >
+          My Account
+        </Typography>
+        <NavTabs />
+      </Container>
+    </div>
+  );
+}

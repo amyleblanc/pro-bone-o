6b909ad08d0e2ad873d2a8ede3297d38ee0f3f9b
RhysWood <rhyswoody@gmail.com>
2022-05-13 19:59:12 -0700

    add image to landing page

--- react-front-end/src/pages/splash.js
+++ react-front-end/src/pages/splash.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button, Grid, Typography, Container } from "@mui/material/";
 import { Link } from "react-router-dom";
+import { ReactComponent as DogSvG } from './img/undraw_good_doggy_-4-wfq.svg';
+
 
 export default function Splash() {
   return (
     <div>
-      <Container maxWidth="sm" style={{ marginTop: "100px" }}>
+      <Container maxWidth="sm" style={{ marginTop: "50px"}}>
         <Typography
           variant="h2"
           align="center"
@@ -17,20 +19,7 @@ export default function Splash() {
         <Typography variant="h5" align="center" color="textSecondary" paragraph>
           Find free dog walkers & sitters in your area!
         </Typography>
-        {/* <div>
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item>
-              <Link style={{ textDecoration: "none" }} to={"/login"}>
-                <Button variant="contained">Login</Button>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link style={{ textDecoration: "none" }} to={"/register"}>
-                <Button variant="outlined">Register</Button>
-              </Link>
-            </Grid>
-          </Grid>
-        </div> */}
+          <DogSvG />
       </Container>
     </div>
   );

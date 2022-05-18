8a971d79d56e8ae3880ce23d408eaf76516c1623
brysonbest <brycekalden@gmail.com>
2022-05-17 20:25:22 -0700

    Update google maps with rendering listings based on search.

--- react-front-end/src/components/maps/Map.jsx
+++ react-front-end/src/components/maps/Map.jsx
@@ -1,33 +1,53 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useState } from "react";
+import axios from "axios";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import './Map.css';
+import "./Map.css";
 
-export default function Map() {
+export default function Map(props) {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
-
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Mapa />;
-}
-
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-};
-
-function Mapa() {
+  const [listing, setListing] = useState([]);
+  const { url, payload } = props;
   const center = useMemo(() => ({ lat: 49.279115, lng: -123.051945 }), []);
-  const markerTest = useMemo(() => ({ lat: 49.277535969084596, lng: -123.05256805076216 }), []);
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  useEffect(() => {
+    const getSearch = async () => {
+      const res = await axios
+        .post(url, payload)
+        .then((res) => setListing(res.data));
+    };
+    getSearch();
+  }, [url, payload]);
+
+  const useListing = listing.map((listing) => {
+    let title = listing.activity_type + " with ";
+    title += listing.sitter_listing
+      ? listing.pets.name
+      : listing.users.first_name;
+    //console.log(listing);
+    const latitude = 49.277535969084596;
+    const longitude = -123.05256805076216;
+    return (
+      <Marker
+        title={title}
+        name={"SOMA"}
+        position={{ lat: 49.277535969084596, lng: -123.05256805076216 }}
+      />
+    );
+  });
 
   return (
-    <GoogleMap zoom={13} center={center} mapContainerStyle={containerStyle}>
-      <Marker
-        title={'The marker`s title will appear as a tooltip.'}
-        name={'SOMA'}
-        position={markerTest} 
-      />
-    </GoogleMap>
+    <main>
+      <h1>Map of Listings</h1>
+      <GoogleMap zoom={13} center={center} mapContainerStyle={containerStyle}>
+        {listing && useListing}
+      </GoogleMap>
+    </main>
   );
 }
